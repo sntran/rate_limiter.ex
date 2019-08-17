1ce4e6b8b78227c227e0421187e53a3351286cb1
Son Tran-Nguyen <code@sntran.com>
2019-08-17 18:03:16 -0500

    Let router use rate limiter
    Don't know how to test it though...
    Testing a web handler by capturing terminal output just feels ...weird.

--- lib/rate_limiter/router.ex
+++ lib/rate_limiter/router.ex
@@ -1,10 +1,22 @@
 defmodule RateLimiter.Router do
+  import Plug.Conn
   use Plug.Router
 
   plug :match
   plug :dispatch
 
   get "/receive-message" do
+    conn = fetch_query_params(conn)
+    case conn.query_params do
+      %{"message" => message, "queue" => queue} ->
+        worker = fn(_) ->
+          IO.puts message
+        end
+        RateLimiter.enqueue({worker, queue})
+      _params ->
+        :skip
+    end
+    
     send_resp(conn, 200, "ack")
   end
 
